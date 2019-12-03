b169507f29a82e8d41bd59000b60935fd493d69b
jeremtarenio <jeremtarenio@gmail.com>
2019-12-03 16:25:59 +0800

    changed cross origins

--- src/main/java/com/ilp/rest/webservices/restfulwebservices/IngredientResource.java
+++ src/main/java/com/ilp/rest/webservices/restfulwebservices/IngredientResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://jeremtarenio.github.io/recipe-book")
 @RestController
 public class IngredientResource {
 	@Autowired
